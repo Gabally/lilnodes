c83736f9c43e2a52211615312a86dea98fda27da
Gabriele Morisano <gaballyno@gmail.com>
2021-12-03 17:52:36 +0100

    ignore key

--- public/editor.js
+++ public/editor.js
@@ -1,10 +1,27 @@
 const editor = new CodeFlask("#editor", { language: "js" });
-function createNode() {
-    console.log(editor.getCode());
+var dependencies = [];
+
+async function createNode() {
+    let resp = await fetch("/createnode", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            code: editor.getCode(),
+            dependencies: dependencies
+        })
+    });
+    let { success, url } = await resp.json();
+    console.log(success, url); 
 }
 
 function $(selector) {
     return document.querySelector(selector);
+}
+
+function newElement(name) {
+    return document.createElement(name);
 }
 
 window.onload = function() {
@@ -12,3 +29,21 @@ window.onload = function() {
         createNode();
     });
 }
+
+function addDependency() {
+    let dep = $("#dep-input");
+    if (dep.value) {
+        dependencies.push(dep.value);
+        dep.value = "";
+        let container = $("#dep-list");
+        container.innerHTML = "";
+        dependencies.forEach(d => {
+            let depDiv = newElement("div");
+            depDiv.className = "dep";
+            depDiv.textContent = d;
+            container.appendChild(depDiv);
+        });
+    }
+}
+
+$("#add-dep").addEventListener("click", addDependency);
